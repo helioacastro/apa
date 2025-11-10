67b33208dd12a01c4adecb99d351acd92c113e8a
Helio Araujo e Castro <63274045+helioacastro@users.noreply.github.com>
2025-11-10 18:21:11 -0300

    Alteracao de pastas

--- alomundo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Imprime a string "Olá Mundo" no console
-    printf("Olá Mundo\n");
-    
-    // Retorna 0 para indicar que o programa foi executado com sucesso
-    return 0;
-}
